b4f072e74e200cfd173344ee09855435304b22d7
やにけ <137967446+yanikee@users.noreply.github.com>
2024-10-14 01:12:28 +0000

    賭け金聞くとこまで

--- main.ts
+++ main.ts
@@ -1,21 +1,32 @@
-var money = 5;
+const data = {
+  money: 5,
+  bet: 0
+};
 
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-function how_much_bet() {
-  readline.question('いくら賭けますか？ ', (answer) => {
-    console.log(`入力値: $${answer}`);
-    readline.close();
-  });
-}
 
 function start() {
-  console.log(`あなたの所持金は$${money}です。`)
-  const answer = how_much_bet();
-  console.log(answer);
+  console.log(`あなたの所持金は$${data.money}です。`)
+
+  function askBet() {
+    readline.question('いくら賭けますか？ ', (bet) => {
+      data.bet = parseInt(bet)
+      console.log(`入力値: $${bet}`);
+
+      if (0 < bet && bet <= data.money) {
+        readline.close();
+      } else{
+        console.log("適切な整数を入力してください")
+        askBet()
+      }
+    });
+  }
+  askBet()
 }
+
 
 start()
