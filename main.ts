43e9cc3045463a4cc2cedbd42f53eebb638b1cc1
やにけ <137967446+yanikee@users.noreply.github.com>
2024-10-14 01:13:33 +0000

    コメントアウト追加追加

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ function start() {
     readline.question('いくら賭けますか？ ', (bet) => {
       data.bet = parseInt(bet)
       console.log(`入力値: $${bet}`);
-
+      // 入力された賭け金が、0以上所持金以下だった場合のみOK
       if (0 < bet && bet <= data.money) {
         readline.close();
       } else{
