5cf59b25ae7f55f9831f74b824a685b13b86f680
やにけ <137967446+yanikee@users.noreply.github.com>
2024-10-14 03:06:30 +0000

    A,J,Q,Kにした

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const data = {
   money: 5,
   bet: 0,
-  dealer: [],
-  player: []
+  dealer: [[], []], //[1,2,3,...,13], [A,2,3,...,K]]
+  player: [[], []], //[1,2,3,...,13], [A,2,3,...,K]]
 };
 
 const readline = require('readline').createInterface({
@@ -29,7 +29,21 @@ function askBet() {
 function dealCards() {
   ["dealer", "player"].forEach((participant) => {
     for (let i = 0; i < 2; i++) {
-      data[participant].push(Math.floor(Math.random() * 13) + 1);
+      const num = Math.floor(Math.random() * 13) + 1;
+      let card = num
+      if (num == 1) {
+        card = "A"
+      } else if (num == 10) {
+        card = "J"
+      } else if (num == 11) {
+        card = "Q"
+      } else if (num == 12) {
+        card = "k"
+      } else {
+        card = num
+      }
+      data[participant][0].push(num);
+      data[participant][1].push(card);
     }
   });
 }
@@ -39,11 +53,12 @@ async function startGame() {
   data.bet = await askBet();
   data.money -= data.bet;
   console.log(`入力された賭け金: $${data.bet}`)
+  readline.close();
 
   dealCards();
 
-  console.log(data);
-  readline.close();
+  console.log(`ディーラー\n　1枚目: ${data.dealer[1][0]}\n　2枚目: 秘密`);
+  console.log(`プレイヤー\n　1枚目: ${data.player[1][0]}\n　2枚目: ${data.player[1][1]}`);
 }
 
 startGame()
